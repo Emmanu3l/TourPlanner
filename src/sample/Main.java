b9c68d3e98267503fef09175668a18825aa6028d
Emmanuel Najfar <if20b200@technikum-wien.at>
2021-03-10 20:50:15 +0100

    Add simple Button and ActionEvent

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,44 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+    Button button;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        button = new Button("Start"); //create button and set label
+        button.setOnAction(this); //whenever the user clicks the button the code to handle it is in *this* class, instead of a separate one
+
+        StackPane layout = new StackPane(); //simple layout
+        layout.getChildren().add(button); //add button to layout
+
+        primaryStage.setTitle("Tour Planner");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(layout, 300, 275));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) { //handle user event
+        if (actionEvent.getSource() == button) { //check which button was pressed
+            System.out.println("test");
+        }
     }
 }
