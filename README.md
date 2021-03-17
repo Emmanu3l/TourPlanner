3a34580c97fb44a1fe728d5994c0f10aebd164b9
Emmanuel Najfar <if20b200@technikum-wien.at>
2021-03-17 12:55:38 +0100

    Add stubs

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ implementieren eines simplen Layouts und Buttons (ca. 4 Stunden)
 11.03.2020: Experimentieren mit Lambda Expressions und BorderPane
 
 16.03.2020: Erstmaliges experimentieren mit FXML (ca. 3 Stunden)
+17.03.2020: Etwas mehr Feinschliff beim fxml file
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,8 +6,43 @@ public class Controller {
 
     public Button search;
 
+    public Button addTour;
+    public Button removeTour;
+    public Button modifyTour;
+
+    public Button addLog;
+    public Button removeLog;
+    public Button modifyLog;
+
+
     public void handleSearch() {
         search.setText("Results found");
+    }
+
+
+    public void handleAddLog() {
+        System.out.println("pressed");
+    }
+
+    public void handleRemoveLog() {
+        System.out.println("pressed");
+    }
+
+    public void handleModifyLog() {
+        System.out.println("pressed");
+    }
+
+
+    public void handleAddTour() {
+        System.out.println("pressed");
+    }
+
+    public void handleRemoveTour() {
+        System.out.println("pressed");
+    }
+
+    public void handleModifyTour() {
+        System.out.println("pressed");
     }
 
 }
