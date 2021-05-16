3bd8f80e678aaac4aacb2a10e5cf7606702a85ff
Emmanuel Najfar <if20b200@technikum-wien.at>
2021-05-16 12:54:12 +0200

    Create DatabaseTest.java

--- src/views/MainWindowController.java
+++ src/views/MainWindowController.java
@@ -28,8 +28,7 @@ public class MainWindowController implements Initializable {
     //TODO: HTTP for MapQuest
     //TODO: Logginglog4j
     //TODO: report-generation library of choice
-    //TODO: documentation annotations with doxygen
-    //TODO: iText PDF generation
+    //TODO: documentation annotation/generation with doxygen [OPTIONAL]
 
     //TODO: [NameGenerator.java] possibly remove single and three letter syllables so you can guarantee a certain length
     //TODO: [Database.java] ADD CREATE, DROP, SELECT STATEMENTS TO DAO CLASSES AND MAKE THEM ACCESSIBLE AS A FUNCTION
---
+++ test/dataaccesslayer/postgresSqlServer/DatabaseTest.java
@@ -0,0 +1,18 @@
+package dataaccesslayer.postgresSqlServer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @org.junit.jupiter.api.Test
+    void insertNew() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void tourReader() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTourReader() {
+    }
+}
